a19ad82e939a4bebb8c4d7ceb23e75933495a3f4
Eng-Amir-M-Hamza <amirautodeskacc4@gmail.com>
2026-04-19 00:16:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Recruiter Killer v2 Loaded");
+const sections = document.querySelectorAll(".reveal");
+
+function reveal() {
+sections.forEach(sec => {
+const top = sec.getBoundingClientRect().top;
+
+if (top < window.innerHeight - 120) {
+sec.classList.add("show");
+}
+});
+}
+
+window.addEventListener("scroll", reveal);
+reveal();
+
+console.log("FAANG Ultra Cinematic Portfolio Loaded");
