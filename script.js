2f6d31733d9d26a0c2b14330cb21e036834655dd
Eng-Amir-M-Hamza <amirautodeskacc4@gmail.com>
2026-04-18 22:29:50 +0200

    Refactor modal content handling for various types

--- script.js
+++ script.js
@@ -1,43 +1,27 @@
 function openModal(type){
-    let content = "";
 
-    if(type === "addin1"){
-        content = `
-        <h2>ToolBox Add-in</h2>
-        <p>Automates Revit workflows and improves productivity.</p>
-        <img src="images/addins/toolbox.png">
-        <button onclick="window.open('https://youtube.com')">Watch Demo</button>
-        `;
-    }
+let content = "";
 
-    if(type === "dynamo1"){
-        content = `
-        <h2>Dynamo Script</h2>
-        <p>Automates shop drawing generation.</p>
-        <img src="images/dynamo/script1.png">
-        `;
-    }
+if(type === "ms"){
+content = `
+<h2>Microsoft Data Centers</h2>
+<p>LOD 400 BIM execution models, clash-free coordination.</p>
+<img src="images/projects/dc.png">
+`;
+}
 
-    if(type === "dc"){
-        content = `
-        <h2>Microsoft Data Centers</h2>
-        <p>LOD 400 BIM execution models & coordination.</p>
-        <img src="images/projects/dc.png">
-        `;
-    }
+if(type === "hp"){
+content = `
+<h2>Magdy Yacoub Hospital</h2>
+<p>X-ray rooms electrical design & coordination.</p>
+<img src="images/projects/hospital.png">
+`;
+}
 
-    if(type === "hospital"){
-        content = `
-        <h2>Magdy Yacoub Hospital</h2>
-        <p>Electrical systems for X-ray rooms.</p>
-        <img src="images/projects/hospital.png">
-        `;
-    }
-
-    document.getElementById("modal-body").innerHTML = content;
-    document.getElementById("modal").style.display = "flex";
+document.getElementById("modal-body").innerHTML = content;
+document.getElementById("modal").style.display = "flex";
 }
 
 function closeModal(){
-    document.getElementById("modal").style.display = "none";
+document.getElementById("modal").style.display = "none";
 }
