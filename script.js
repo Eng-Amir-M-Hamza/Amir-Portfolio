231265b02a95ad1f86255daacc1aedbc23177c3b
Eng-Amir-M-Hamza <amirautodeskacc4@gmail.com>
2026-04-18 23:22:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-const sections = document.querySelectorAll(".section");
-
-/* SAFE: no invisible bugs */
-const reveal = () => {
-sections.forEach(sec => {
-sec.style.opacity = 1;
-sec.style.transform = "translateY(0)";
-sec.style.transition = "1s ease";
-});
-};
-
-reveal();
-
-console.log("God Mode v2 Stable Cinematic Loaded");
+console.log("Portfolio Loaded - Stable Mode");
