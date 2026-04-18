971841899c0bc9ad4f3f37b716f9ab5affc33d30
Eng-Amir-M-Hamza <amirautodeskacc4@gmail.com>
2026-04-18 22:55:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Portfolio Loaded Successfully");
+const sections = document.querySelectorAll(".fade");
+
+window.addEventListener("scroll", () => {
+sections.forEach(sec => {
+const pos = sec.getBoundingClientRect().top;
+
+if(pos < window.innerHeight - 100){
+sec.classList.add("show");
+}
+});
+});
+
+console.log("Hollywood Portfolio Loaded");
