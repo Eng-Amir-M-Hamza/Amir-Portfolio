d873592c168a025af4c7257fe773ae06625f7d65
Eng-Amir-M-Hamza <amirautodeskacc4@gmail.com>
2026-04-18 22:47:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Apple-level portfolio loaded");
+function revealOnScroll() {
+const elements = document.querySelectorAll(".reveal, .card");
+
+for (let i = 0; i < elements.length; i++) {
+let windowHeight = window.innerHeight;
+let elementTop = elements[i].getBoundingClientRect().top;
+let elementVisible = 100;
+
+if (elementTop < windowHeight - elementVisible) {
+elements[i].classList.add("active");
+}
+}
+}
+
+window.addEventListener("scroll", revealOnScroll);
