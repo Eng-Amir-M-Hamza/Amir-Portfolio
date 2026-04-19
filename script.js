19746bd4dceba021e4e2a0999fa4c82c30dcd1bf
Eng-Amir-M-Hamza <amirautodeskacc4@gmail.com>
2026-04-19 22:37:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-function openProject(page){
-    window.location.href = page;
+// ===== THEME TOGGLE =====
+const toggleBtn = document.getElementById("themeToggle");
+
+// Load saved mode
+if (localStorage.getItem("theme") === "light") {
+    document.body.classList.add("light-mode");
+}
+
+// Toggle on click
+if (toggleBtn) {
+    toggleBtn.addEventListener("click", () => {
+        document.body.classList.toggle("light-mode");
+
+        if (document.body.classList.contains("light-mode")) {
+            localStorage.setItem("theme", "light");
+        } else {
+            localStorage.setItem("theme", "dark");
+        }
+    });
 }
